5bb9a2bb0afe182d45d33a4a267ba684ba89678f
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2024-02-27 11:50:53 +0900

    Created:Pages List

--- lib/Screens/mainscreen.dart
+++ lib/Screens/mainscreen.dart
@@ -1,51 +1,53 @@
+import 'package:ecommerce/Screens/cartscreen.dart';
+import 'package:ecommerce/Screens/homescreen.dart';
+import 'package:ecommerce/Screens/personscreen.dart';
+import 'package:ecommerce/Screens/searchscreen.dart';
 import 'package:ecommerce/constants/appstyle.dart';
 import 'package:flutter/material.dart';
+import 'package:ecommerce/Widgets/bottomnav.dart';
+import 'package:ecommerce/Controllers/providercontroller.dart';
+import 'package:provider/provider.dart';
+
 
 class MainScreen extends StatelessWidget {
+  List<Widget> pagelist=[
+    homeScreen(),
+    searchScreen(),
+    homeScreen(),
+    cartScreen(),
+    personScreen(),
+   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:Center(
-        child:Text('HomeScreen',
-        style:appstyle(40, Colors.black, FontWeight.bold)),
-      ),
-      bottomNavigationBar: SafeArea(
-        child: Padding(padding: EdgeInsets.all(8),
-        child:Container(
-          padding: EdgeInsets.all(12),
-          margin:EdgeInsets.symmetric(horizontal: 10),
-          decoration: BoxDecoration(
-            color:Colors.black,
-            borderRadius: BorderRadius.circular(8)),
-            child:Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                bottomnav(iconitem: Icons.home, ontap:(){},),
-                bottomnav(iconitem: Icons.home, ontap:(){},),
-                bottomnav(iconitem: Icons.home, ontap:(){},),
-                bottomnav(iconitem: Icons.home, ontap:(){},),
-                bottomnav(iconitem: Icons.home, ontap:(){},),
-                  ],
-                ),
+    return Consumer<MainScreenNotifier>(
+      builder: (context, mainscreennotifier , child){
+      return Scaffold(
+        body:pagelist[mainscreennotifier.pageindex],
+        bottomNavigationBar: SafeArea(
+          child: Padding(padding: EdgeInsets.all(8),
+          child:Container(
+            padding: EdgeInsets.all(12),
+            margin:EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+              color:Colors.black,
+              borderRadius: BorderRadius.circular(8)),
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  bottomnav(iconitem: Icons.home, ontap:(){mainscreennotifier.setpageindex=0;},),
+                  bottomnav(iconitem: Icons.search, ontap:(){mainscreennotifier.setpageindex=1;},),
+                  bottomnav(iconitem: Icons.add, ontap:(){mainscreennotifier.setpageindex=2;},),
+                  bottomnav(iconitem: Icons.shopping_basket, ontap:(){mainscreennotifier.setpageindex=3;},),
+                  bottomnav(iconitem: Icons.person, ontap:(){mainscreennotifier.setpageindex=4;},),
+                    ],
+                  ),
+            ),
           ),
-          
-          
-        )
-        ),
-        );
+            
+          ),
+          );
+      }
+          );
   }
 }
 
-class bottomnav extends StatelessWidget {
-  final Function? ontap;
-  final IconData? iconitem;
-
-  bottomnav({required this.ontap,required this.iconitem});
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      child: IconButton(icon:Icon(iconitem,color: Colors.white,),
-      onPressed: ()=>ontap),
-      );
-}
-}
