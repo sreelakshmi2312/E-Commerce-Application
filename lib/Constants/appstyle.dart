efab22b02012b42576ddad0d7a7a7886fe1e2497
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2024-02-27 09:32:58 +0900

    Added:Common appstyle file

---
+++ lib/Constants/appstyle.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle appstyle(double fontsize , Color iconcolor , FontWeight fontweight){
+  return GoogleFonts.poppins(fontSize: fontsize,color:iconcolor,fontWeight: fontweight);
+}
