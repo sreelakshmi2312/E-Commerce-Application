6d0894c2d74e79c489ebbfba926113a7a201677c
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2024-02-27 11:49:27 +0900

    Added:cart screen

---
+++ lib/Screens/cartscreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:ecommerce/Widgets/bottomnav.dart';
+import 'package:ecommerce/Constants/appstyle.dart';
+
+class cartScreen extends StatefulWidget {
+  const cartScreen({super.key});
+
+  @override
+  State<cartScreen> createState() => _cartScreenState();
+}
+
+class _cartScreenState extends State<cartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Center(
+        child:Text('cart Screen',
+        style:appstyle(40, Colors.black, FontWeight.bold)),
+      ),
+        );
+  }
+}
